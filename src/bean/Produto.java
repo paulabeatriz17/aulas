aae157a5b6384ddddd9a946acaef87b2f67f60bb
paulabeatriz17 <paula.beatriz.benites@gmail.com>
2023-08-28 23:09:17 -0300

    Second Commint

--- src/bean/Produto.java
+++ src/bean/Produto.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package bean;
 
 /**
@@ -10,5 +6,5 @@ package bean;
  * @author paula
  */
 public class Produto {
-    
+    String nome;
 }
